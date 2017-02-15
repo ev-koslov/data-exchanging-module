5dc821f8f378618fc056949cc7c9b498f61a0a8c
Eugene Koslov <ev.koslov@gmail.com>
2017-02-15 17:40:58 +0200

    !

--- src/main/java/ev/koslov/data_exchanging/components/tags/StatusTag.java
+++ src/main/java/ev/koslov/data_exchanging/components/tags/StatusTag.java
@@ -4,5 +4,6 @@ package ev.koslov.data_exchanging.components.tags;
  * Taglib that shows {@link ev.koslov.data_exchanging.components.Message} status
  */
 public enum StatusTag {
-    OK, ERROR, DENIED, TIMEOUT, PARTIAL_DATA
+    //TODO: add processing for whole tags
+    OK, ERROR, DENIED, TIMEOUT, PARTIAL_DATA, PROCESSING_TIMEOUT
 }
